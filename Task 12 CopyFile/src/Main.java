3c3e3a0968f52475785108a8853d4f2031915ab9
3idjn7 <seprefy@gmail.com>
2023-03-12 22:42:44 +0200

    Create a program that copies a file. The program will receive file to copy (source) and file name to copy the file into (destination). Implement the program using 4 different approaches: Implement the program using FileInputStream and FileOutputStream. The file should be copied byte by byte. Implement the program using BufferedInputStream and BufferedOutputStream. The file should be copied using a buffer with fixed size. Implement the program using FileChannel class. Implement the program using Files.copy static method. Test the program with file that is at least 10 MB. Measure the time spent to copy the file for all different approaches.

--- Task 12 CopyFile/src/Main.java
+++ Task 12 CopyFile/src/Main.java
@@ -4,66 +4,91 @@ import java.nio.file.*;
 import java.time.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String sourceFile = args[0];
         String destinationFile = args[1];
 
         System.out.println("Copying file using FileInputStream and FileOutputStream...");
-        Instant start = Instant.now();
-        FileInputStream in = null;
-        FileOutputStream out = null;
-        try {
-            in = new FileInputStream(sourceFile);
-            out = new FileOutputStream(destinationFile);
-            int c;
-            while ((c = in.read()) != -1) {
-                out.write(c);
+        long timeTaken = measureTime(() -> {
+            try {
+                copyFileUsingStreams(sourceFile, destinationFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } finally {
-            if (in != null) in.close();
-            if (out != null) out.close();
-        }
-        Instant end = Instant.now();
-        System.out.println("Time taken: " + Duration.between(start, end).toMillis() + " ms");
+        });
+        System.out.printf("Time taken: %d ms%n%n", timeTaken);
 
         System.out.println("Copying file using BufferedInputStream and BufferedOutputStream...");
-        start = Instant.now();
-        BufferedInputStream bis = null;
-        BufferedOutputStream bos = null;
-        try {
-            bis = new BufferedInputStream(new FileInputStream(sourceFile));
-            bos = new BufferedOutputStream(new FileOutputStream(destinationFile));
-            byte[] buffer = new byte[8192];
-            int count;
-            while ((count = bis.read(buffer)) != -1) {
-                bos.write(buffer, 0, count);
+        timeTaken = measureTime(() -> {
+            try {
+                copyFileUsingBufferedStreams(sourceFile, destinationFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } finally {
-            if (bis != null) bis.close();
-            if (bos != null) bos.close();
-        }
-        end = Instant.now();
-        System.out.println("Time taken: " + Duration.between(start, end).toMillis() + " ms");
+        });
+        System.out.printf("Time taken: %d ms%n%n", timeTaken);
 
         System.out.println("Copying file using FileChannel class...");
-        start = Instant.now();
-        FileChannel sourceChannel = null;
-        FileChannel destChannel = null;
-        try {
-            sourceChannel = new FileInputStream(sourceFile).getChannel();
-            destChannel = new FileOutputStream(destinationFile).getChannel();
-            destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
-        } finally {
-            if (sourceChannel != null) sourceChannel.close();
-            if (destChannel != null) destChannel.close();
-        }
-        end = Instant.now();
-        System.out.println("Time taken: " + Duration.between(start, end).toMillis() + " ms");
+        timeTaken = measureTime(() -> {
+            try {
+                copyFileUsingChannels(sourceFile, destinationFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.printf("Time taken: %d ms%n%n", timeTaken);
 
         System.out.println("Copying file using Files.copy static method...");
-        start = Instant.now();
+        timeTaken = measureTime(() -> {
+            try {
+                copyFileUsingFilesCopy(sourceFile, destinationFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.printf("Time taken: %d ms%n%n", timeTaken);
+    }
+
+    private static long measureTime(Runnable action) {
+        Instant start = Instant.now();
+        try {
+            action.run();
+        } finally {
+            Instant end = Instant.now();
+            return Duration.between(start, end).toMillis();
+        }
+    }
+
+    private static void copyFileUsingStreams(String sourceFile, String destinationFile) throws IOException {
+        try (InputStream in = new FileInputStream(sourceFile);
+             OutputStream out = new FileOutputStream(destinationFile)) {
+            byte[] buffer = new byte[8192];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+        }
+    }
+
+    private static void copyFileUsingBufferedStreams(String sourceFile, String destinationFile) throws IOException {
+        try (InputStream in = new BufferedInputStream(new FileInputStream(sourceFile));
+             OutputStream out = new BufferedOutputStream(new FileOutputStream(destinationFile))) {
+            byte[] buffer = new byte[8192];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+        }
+    }
+
+    private static void copyFileUsingChannels(String sourceFile, String destinationFile) throws IOException {
+        try (FileChannel in = new FileInputStream(sourceFile).getChannel();
+             FileChannel out = new FileOutputStream(destinationFile).getChannel()) {
+            out.transferFrom(in, 0, in.size());
+        }
+    }
+
+    private static void copyFileUsingFilesCopy(String sourceFile, String destinationFile) throws IOException {
         Files.copy(Paths.get(sourceFile), Paths.get(destinationFile), StandardCopyOption.REPLACE_EXISTING);
-        end = Instant.now();
-        System.out.println("Time taken: " + Duration.between(start, end).toMillis() + " ms");
     }
 }
