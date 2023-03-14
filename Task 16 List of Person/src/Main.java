67c495a1f1ccaff6eaedef8c70944190f35c4bb6
3idjn7 <seprefy@gmail.com>
2023-03-14 22:32:47 +0200

    Create a program that sorts a List of Person based on different criteria. Follow the steps bellow: Create a class Person, with fields firstName, lastName, address and age. Override toString method to display Person fields information. Create a method that sorts a list of people by the firstName. Implement Comparable interface in Person class and override the corresponding compareTo(T o) method. Use Collections.sort method to sort the elements. Create a method that sorts a list of people by their age. This time create a separate class that implements the Comparator interface and overrides compare(T o1, T o2) method. Pass that class to Collections.sort method to sort the elements. Create a method that sorts a list of people by their address. Implement the Comparator interface via anonymous class. Write a main method to test all 3 sorting  methods.

---
+++ Task 16 List of Person/src/Main.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class Person implements Comparable<Person> {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int age;
+
+    public Person(String firstName, String lastName, String address, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person- " +
+                "First Name: '" + firstName + '\'' +
+                ", Last Name: '" + lastName + '\'' +
+                ", Address: '" + address + '\'' +
+                ", Age: " + age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.firstName.compareTo(o.getFirstName());
+    }
+}
+
+class AgeComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("John", "Doe", "Tzar Osvoboditel 121", 30));
+        people.add(new Person("Jane", "Doe", "Tzar Osvoboditel 123", 30));
+        people.add(new Person("Joseph", "Doe", "Tzar Osvoboditel 122", 11));
+
+        Collections.sort(people);
+        System.out.println("Sorted by first name:");
+        System.out.println(people);
+
+        Collections.sort(people, new AgeComparator());
+        System.out.println("Sorted by age:");
+        System.out.println(people);
+
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAddress().compareTo(o2.getAddress());
+            }
+        });
+        System.out.println("Sorted by address:");
+        System.out.println(people);
+    }
+}
