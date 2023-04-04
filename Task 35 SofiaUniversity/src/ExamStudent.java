93171dc9e56cc46f4cca44bb46ca691a5283b390
3idjn7 <seprefy@gmail.com>
2023-04-04 20:52:22 +0300

    Added reflection example.

--- Task 35 SofiaUniversity/src/ExamStudent.java
+++ Task 35 SofiaUniversity/src/ExamStudent.java
@@ -1,6 +1,8 @@
 import university.*;
 
 import java.util.ArrayList;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ExamStudent {
     public static void main(String[] args) {
@@ -17,5 +19,21 @@ public class ExamStudent {
         teacher.getExams().add(exam);
 
         teacher.giveExam(student);
+
+        try {
+            // Get the private answerQuestion method of the Student class
+            Method method = Student.class.getDeclaredMethod("answerQuestion", Question.class);
+
+            // Make the method accessible
+            method.setAccessible(true);
+
+            // Call the method on the student object with a new Question object
+            int answer = (int) method.invoke(student, new Question("What is 2+2", 4));
+
+            System.out.println("Answer: " + answer);
+
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
