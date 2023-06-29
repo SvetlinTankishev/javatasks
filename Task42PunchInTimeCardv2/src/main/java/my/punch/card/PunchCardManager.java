b9dea781b2732843c7df495cf993574d277faf62
3idjn7 <seprefy@gmail.com>
2023-06-29 15:50:31 +0300

    Added tests wip junit mockito

--- Task42PunchInTimeCardv2/src/main/java/my/punch/card/PunchCardManager.java
+++ Task42PunchInTimeCardv2/src/main/java/my/punch/card/PunchCardManager.java
@@ -26,11 +26,6 @@ public class PunchCardManager {
 
     public void setFileName() {
     }
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> origin/main
     private void createFilesIfNotExist() {
         try {
             Path of1 = Path.of(FILE_NAME);
